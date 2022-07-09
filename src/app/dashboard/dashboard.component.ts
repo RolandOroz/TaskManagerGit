941a16766d5b3553e78c5b1bbee3eb369ef0ec89
RolandOroz <rolandoroz@gmail.com>
2022-07-09 10:25:27 +0200

    making collapsable accordions east, south, west, north

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -39,5 +39,55 @@ export class DashboardComponent implements OnInit {
       "DEF Software Solutions",
       "GHI Industries"
     ];
+
+    this.Projects = [
+      "Project A",
+      "Project B",
+      "Project C",
+      "Project D",
+    ];
+
+    for (let i = 2019; i >= 2010; i--) {
+      this.Years.push(i);
+    }
+
+    this.TeamMembersSummary = [
+      {Region: "East", TeamMembersCount: 20, TemporarilyUnavailableMembers: 4},
+      {Region: "West", TeamMembersCount: 15, TemporarilyUnavailableMembers: 8},
+      {Region: "South", TeamMembersCount: 17, TemporarilyUnavailableMembers: 1},
+      {Region: "North", TeamMembersCount: 15, TemporarilyUnavailableMembers: 6},
+    ];
+
+    this.TeamMembers = [
+
+      {Region: "East", Members: [
+          {ID: 1, Name: "Ford", Status: "Available"},
+          {ID: 2, Name: "Miller", Status: "Available"},
+          {ID: 3, Name: "Jones", Status: "Busy"},
+          {ID: 4, Name: "James", Status: "Busy"},
+        ]
+      },
+      {Region: "West", Members: [
+          {ID: 5, Name: "Anna", Status: "Available"},
+          {ID: 6, Name: "Jasmine", Status: "Busy"},
+          {ID: 7, Name: "Jim", Status: "Available"},
+          {ID: 8, Name: "George", Status: "Busy"},
+        ]
+      },
+      {Region: "South", Members: [
+          {ID: 9, Name: "Luce", Status: "Available"},
+          {ID: 10, Name: "Max", Status: "Available"},
+          {ID: 11, Name: "Jacob", Status: "Available"},
+          {ID: 12, Name: "Emily", Status: "Busy"},
+        ]
+      },
+      {Region: "North", Members: [
+          {ID: 13, Name: "Frank", Status: "Available"},
+          {ID: 14, Name: "Megan", Status: "Available"},
+          {ID: 15, Name: "Sharon", Status: "Busy"},
+          {ID: 16, Name: "Tim", Status: "Busy"},
+        ]
+      },
+    ]
   }
 }
