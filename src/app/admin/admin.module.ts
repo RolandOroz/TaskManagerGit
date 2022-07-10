30715e18cb41ce67e51fb2a4afbfb7a7cb6b44d8
RolandOroz <rolandoroz@gmail.com>
2022-07-10 20:48:50 +0200

    creating new module Admin, refactoring folders: about, my-profile and dashboard into admin folder. AdminModule has exports: DashboardComponent, AboutComponent, MyProfileComponent and declarations: DashboardComponent, AboutComponent, MyProfileComponent also new imports of: AppRoutingModule. AppModule has new imports of: AdminModule

---
+++ src/app/admin/admin.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import {DashboardComponent} from "./dashboard/dashboard.component";
+import {AboutComponent} from "./about/about.component";
+import {MyProfileComponent} from "./my-profile/my-profile.component";
+import {AppRoutingModule} from "../app-routing.module";
+
+
+
+@NgModule({
+  declarations: [
+    DashboardComponent,
+    AboutComponent,
+    MyProfileComponent
+  ],
+  imports: [
+    CommonModule,
+    AppRoutingModule
+  ],
+  exports: [
+    DashboardComponent,
+    AboutComponent,
+    MyProfileComponent
+  ]
+})
+export class AdminModule { }
